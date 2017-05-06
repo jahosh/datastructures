c2745fddb594d98ebc6706767bbfdf7d0260c4ea
=josh91hickman <josh91hickman@gmail.com>
2017-05-05 22:13:00 -0700

    2 tests passing

--- test/linkedList.test.js
+++ test/linkedList.test.js
@@ -1,14 +1,29 @@
 const chai = require('chai');
+const mocha = require('mocha');
 const assert = chai.assert;
+const expect = chai.expect;
 
 const LinkedList = require('../src/linkedList');
 
 
 describe('Linked List', function() {
-  it('should initialize properly', function() {
-    const linkedList = new LinkedList();
+  let linkedList;
+  beforeEach(function() {
+    linkedList = new LinkedList();
+  });
 
+  it('should initialize properly', function() {
     assert.isNotNull(linkedList, 'initialize properly');
   });
+
+  it('should have a head and a tail', function() {
+    expect(linkedList).to.have.property('head');
+    expect(linkedList).to.have.property('tail');
+  });
+
+  // it('should add a value to head correctly', function() {
+  //   linkedList.addToHead(10);
+  //   assert.isNotNull(linkedList.contains(10));
+  // });
 });
 
